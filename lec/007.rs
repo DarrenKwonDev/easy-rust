508a1e3856bcad534213e5d7f8b8ee816fc32536
drkup <darrenkwondev46@gmail.com>
2024-10-19 23:52:35 +0900

    7, 8, 9

---
+++ lec/007.rs
@@ -0,0 +1,8 @@
+fn give_age() -> i32 {
+    return 42
+}
+
+fn main() {
+    let name = "david";
+    println!("{name} {}", give_age());
+}
---
+++ lec/008.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let city = "seoul";
+    let year = 2024;
+    let pop: i64 = 13_545_343_456;
+
+    // 순서를 지정할 수도
+    println!("city {1} in {0}, pop {2}", year,city, pop);
+}
