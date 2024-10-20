9ce945f352456ba47bb88c871cccf9563f618dd7
drkup <darrenkwondev46@gmail.com>
2024-10-20 12:32:40 +0900

    14

--- README.md
+++ README.md
@@ -11,9 +11,24 @@ cargo install cargo-watch
 cargo watch -x run
 ```
 
-
 ## learn things
 
 1. rust에서 char는 32비트 (4바이트) -> 유니코드 지원을 위함.
 2. 블록 마지막 표현식이 세미콜론 없이 작성되면 암시적으로 반환값이 됨
 3. Rust는 다중 레벨의 레퍼런스를 허용합니다.
+
+## others
+
+```text
+[String]
+힙에 할당된 문자열로, 소유권을 가집니다. 변수가 스코프를 벗어나면 자동으로 메모리가 해제됩니다.
+소유권 규칙에 따라 관리됩니다.
+
+[&str]
+문자열 슬라이스로, 다른 문자열 데이터(String이나 정적 문자열)를 참조합니다. 소유권이 없습니다.
+명시적인 라이프타임 지정이 필요할수도 있음
+
+[변환]
+String에서 &str로: &를 사용하여 쉽게 변환 가능 (&String은 &str로 자동 변환)
+&str에서 String으로: to_string() 또는 String::from()을 사용
+```
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,24 @@
 
+/*
+    [String]
+    힙에 할당된 문자열로, 소유권을 가집니다. 변수가 스코프를 벗어나면 자동으로 메모리가 해제됩니다.
+    소유권 규칙에 따라 관리됩니다.
+
+    [&str]
+    문자열 슬라이스로, 다른 문자열 데이터(String이나 정적 문자열)를 참조합니다. 소유권이 없습니다.
+    명시적인 라이프타임 지정이 필요할수도 있음
+
+    [변환]
+    String에서 &str로: &를 사용하여 쉽게 변환 가능 (&String은 &str로 자동 변환)
+    &str에서 String으로: to_string() 또는 String::from()을 사용
+*/
 fn main() {
-    print!("this");
-    print!("this\n");
-    print!("this\n");
+    let mut name = "David".to_string();
+    name.push_str(" cho");
 
-    // raw text. r# ... #
-    println!(r#"C:\Users\drkup\Documents\priceless"#);
-
-    // second와 first 사이의 tab 또한 분리되어서 보임.
-    // 붙여서 쓰고 싶다면 third 처럼 붙여야 함.
-    println!("first
-    second
-third");
-
-    let a = &10;
-    println!("{}", a);   // 자동 역참조되어 10 출력
-    println!("{:p}", a); // 실제로 메모리 주소를 출력하기 위해서는 :p 필요
-
-    let b = 11234;
-    println!("{:X}", b); // 16진수
-    println!("{:b}", b); // 이진수
-
-    /*
-        ^ 중앙 < 왼쪽 > 오른쪽
-    */
-    let title = "NEWS";
-    println!("{:-^30}", title); // 30글자, pad with -, center align
-
-    let bar = "|";
-    println!("{: <15}{: >15}", bar, bar); // {15글자 path with 공백, left align} {15글자 path with 공백, right align}
-
-    let city1 = "seoul"; let city2 = "tokyo";
-    println!("{0:-<15}{1:->15}", city1, city2); // {15글자 path with -, left align} {15글자 path with -, right align}
-
+    let mut other_name = String::from("mama");
+    other_name.push('!');
+    
+    println!("{}", name);
+    println!("{}", other_name);
 }
